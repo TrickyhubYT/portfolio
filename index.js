1064a1f3d19a5a57e261c49322f4be92c870deba
Nora#2100 <64680659+TrickyhubYT@users.noreply.github.com>
2022-08-12 11:36:11 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,85 @@
+/* Made by 3urobeat: https://github.com/HerrEurobeat/website */
+
+/* Enable animations on body: https://stackoverflow.com/a/38296629/12934162 */
+setTimeout(function(){
+    document.body.className="";
+},500);
+
+
+/* Fade in function for our three buttons */
+var opacity = 0;
+
+function fadeIn() {
+    opacity += 0.1
+    document.getElementById("div-buttons").style.opacity = opacity
+
+    setTimeout(() => {
+        if (opacity < 1) fadeIn();
+    }, 20);
+}
+
+/* Let a cursor blink after finishing writing my name */
+function blink() {
+    document.querySelector('.text').innerHTML = str + "&nbsp&nbsp"
+
+    setInterval(() => {
+        document.querySelector('.text').innerHTML = str + "_"
+
+        setTimeout(() => {
+            document.querySelector('.text').innerHTML = str + "&nbsp&nbsp" //print two whitespaces to prevent text from moving
+        }, 750);
+    }, 1250);
+}
+
+/* Helper function to get a random char */
+function randomChar() {
+    return chars[Math.floor(Math.random() * chars.length)]
+}
+
+
+/* Make sure the hamburger menu is collapsed on page load */
+document.getElementById("menu-checkbox").checked = false;
+
+/* Collapse hamburger menu when user clicks outside drawer */
+document.addEventListener('click', (event) => {
+    if (!event.composedPath().includes(document.querySelector('#hamburger-menu'))) {
+        document.getElementById("menu-checkbox").checked = false;
+    }
+})
+
+
+/* Start scrambling animation */
+var frames   = 6;
+var interval = 50;
+var chars    = "!<>-_—=+*^?#0123456789abcdefghijklmnopqrstuvwxyz";
+var text     = "O G G Y";
+
+
+var currentframe = 0
+var i            = 0
+var str          = ""
+
+/* Display random chars frames times and then show a real character */
+var frameinterval = setInterval(() => {
+    document.querySelector('.text').innerHTML = str + randomChar();
+    currentframe++
+
+    if (currentframe >= frames) {
+        str += text[i]
+        currentframe = 0;
+        i++;
+
+        document.querySelector('.text').innerHTML = str
+    } 
+    if (str.length == text.length) {
+        clearInterval(frameinterval)
+
+        blink(); //let cursor blink
+        fadeIn(); //let buttons fade in
+    }
+
+
+
+
+
+}, interval);
